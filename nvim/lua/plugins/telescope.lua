43906f1dcfda6a60cb8657c0f5160ab7979e5812
hibyegoodbye <err0rless313@gmail.com>
2025-12-15 05:21:37 +0900

    feat(nvim): telescope.nvim to ivy layout

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,9 +2,12 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     opts = {
-      defaults = require("telescope.themes").get_ivy({
-        layout_config = { height = 0.5 },
-      }),
+      defaults = {
+        layout_strategy = "ivy",
+        layout_config = {
+          height = 0.5,
+        },
+      },
     },
   },
   {
